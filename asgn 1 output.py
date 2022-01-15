426c55456781f5e6ee4b59896acf095dde5fcc7e
bt21105016-Ramya <97720639+bt21105016-Ramya@users.noreply.github.com>
2022-01-15 13:56:59 +0530

    Add files via upload

---
+++ asgn 1 output.py
@@ -0,0 +1,46 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================================ RESTART: C:\Users\Carrier\Desktop\asg1.py ============================================================
+solving Q1  
+
+enter no 1 14
+enter no 2 76
+enter no 3 47
+sum of given nos is  137
+average of given nos is  45.666666666666664 
+
+solving Q2  
+
+ All values are to be entered in dollars.
+enter your gross income $500000
+enter no of dependents 3
+the tax comes out to be $ 96200.0 
+
+solving Q3  
+
+Student = [sid, name, gender, course, cgpa]
+enter your SID 21105016
+enter your name Ramya
+enter your gender 'M', 'F', 'U' for male, female, unknown respectively.F
+enter your course name ECE
+enter your cgpa 8.9
+student info  [21105016, 'Ramya', 'F', 'ECE', 8.9] 
+
+solving Q4 
+
+enter marks of student 1 76
+enter marks of student 2 86
+enter marks of student 3 79
+enter marks of student 4 88
+enter marks of student 5 69
+marks of 5 students are  [76, 86, 79, 88, 69]
+marks in ascending order  [69, 76, 79, 86, 88]
+marks in descending order  [88, 86, 79, 76, 69] 
+
+solving Q5 
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+ The modified list  is color 1 = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list is color 2 = ['Red', 'Green', 'White', 'Purple', 'Yellow']

9fc0b2a033085e638b3dc6009b261b4d293513e2
bt21105016-Ramya <97720639+bt21105016-Ramya@users.noreply.github.com>
2022-02-13 18:10:11 +0530

    Add files via upload

---
+++ asng 3 output.py
@@ -0,0 +1,126 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\Carrier\Desktop\asng3 q1.py ================
+Question 1
+ input- Phillippines
+Total occurrences
+p-3
+h-1
+i-3
+l-2
+n-1
+e-1
+s-1
+
+================= RESTART: C:\Users\Carrier\Desktop\asng3 q1.py ================
+Question 1
+ input- I scream I sream you scream we all scream for ice cream
+Total occurrences
+i-2
+scream-3
+sream-1
+you-1
+we-1
+all-1
+for-1
+ice-1
+cream-1
+
+================= RESTART: C:\Users\Carrier\Desktop\asng3 q2.py ================
+Question 2
+ enter Day- 28
+ enter Month- 02
+ enter Year- 2022
+Next Date is: 1/3/2022
+
+================= RESTART: C:\Users\Carrier\Desktop\asng3 q2.py ================
+Question 2
+ enter Day- 31
+ enter Month- 12
+ enter Year- 2004
+Next Date is: 1/1/2005
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q2.py ==========================================================
+Question 2
+ enter Day- 28
+ enter Month- 02
+ enter Year- 2020
+Next Date is: 29/2/2020
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q3.py ==========================================================
+Question 3
+[3, 4, 9, 12]
+[(3, 9), (4, 16), (9, 81), (12, 144)]
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asgn3 q4.py ==========================================================
+Question 4
+ Grade Points- 7
+Your Grade is B and Good performance 
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q5.py ==========================================================
+Question 5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q6.py ==========================================================
+Question 6
+Enter SID of Student- 21101008
+Enter the name of the Student- Ankit
+If you want to enter other details press Y/N- Y
+Enter SID of Student- 21105016
+Enter the name of the Student- Ramya
+If you want to enter other details press Y/N- Y
+Enter SID of Student- 21104126
+Enter the name of the Student- Pranav Singh
+If you want to enter other details press Y/N- N
+Part a
+21101008 is Ankit
+21105016 is Ramya
+21104126 is Pranav Singh
+
+Part b
+value sorted dictionary is ['Ankit', 'Pranav Singh', 'Ramya']
+
+Part c
+Keys sorted dictionary is [21101008, 21104126, 21105016]
+
+Part D
+Enter the student's SID whose detail you need- 21105016
+Name of the required student is Ramya
+
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q7.py ==========================================================
+Question 7
+Total elements of Fibonacci sequence (must be greater than 4)- 12
+Fibonnaci sequence with 12 terms
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+Average of total 12 terms of sequence is 19.333333333333332
+
+========================================================== RESTART: C:\Users\Carrier\Desktop\asng3 q8.py ==========================================================
+Question 8
+Part a
+set of all elements that are in Set1 and Set2 but not both. {6, 7, 9}
+Part b
+Set of all elements that are in only one of the three sets Set1, Set2 and Set3. {16, 2, 5, 6, 7, 11}
+Part C
+Set of elements that are only in only one of the three sets Set1, Set2 and Set3. {9, 4}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1. {1, 3, 6, 8, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3. {8, 1, 10, 3}
